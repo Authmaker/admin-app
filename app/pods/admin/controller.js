80b032e98e7bd124c98f6b33913a7bed248a9c35
thedoktor <hey@darraghokeeffe.com>
2015-04-14 18:58:19 +0100

    added button for sending confirmation mail to users

--- app/pods/admin/controller.js
+++ app/pods/admin/controller.js
@@ -2,4 +2,12 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
 
+  actions: {
+    enableUser: function(user) {
+      return Ember.$.post('/api/admin/users/send_confirmation', {
+        username: user.get('username'),
+        email: user.get('email')
+      });
+    }
+  }
 });
