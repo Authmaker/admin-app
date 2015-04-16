a5ce0c32eb375e75a8a9184cf90e4d478b848f81
thedoktor <hey@darraghokeeffe.com>
2015-04-16 12:35:10 +0100

    added notifications to admin app

--- app/pods/admin/controller.js
+++ app/pods/admin/controller.js
@@ -5,9 +5,22 @@ export default Ember.ArrayController.extend({
   actions: {
     enableUser: function(user) {
       return Ember.$.post('/api/admin/users/send_confirmation', {
-        username: user.get('username'),
-        email: user.get('email')
-      });
+        id: user.get('id')
+      }).then(function() {
+        this.notifications.addNotification({
+          message: "Confirmation email sent to " + user.get('email'),
+          type: 'success',
+          autoClear: true
+        });
+      }.bind(this))
+      .then(null, function(err){
+        console.log(err);
+        this.notifications.addNotification({
+          message: 'Error: ' + err.responseText,
+          type: 'error',
+          autoClear: true
+        });
+      }.bind(this));
     }
   }
 });
