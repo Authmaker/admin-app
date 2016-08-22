23ca58461003c89a018c17e503e763a8924b5708
Chris Manson <chris@stonecircle.io>
2016-08-22 09:38:40 +0100

    adding ability to hide a user

--- app/admin/enable/controller.js
+++ app/admin/enable/controller.js
@@ -2,23 +2,40 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+  queryParams: ['showHidden'],
+  showHidden: false,
+
   actions: {
     enableUser: function(user) {
       return Ember.$.post('/api/admin/users/send_confirmation', {
         id: user.get('id')
-      }).then(function() {
+      }).then(() => {
         this.notifications.addNotification({
           message: "Confirmation email sent to " + user.get('email'),
           type: 'success',
           autoClear: true
         });
-      }.bind(this))
-      .then(null, function(err){
+      })
+      .then(null, (err) => {
         this.notifications.addNotification({
           message: 'Error: ' + err.responseText,
           type: 'error'
         });
-      }.bind(this));
-    }
+      });
+    },
+
+    hideUser(user) {
+      return Ember.$.post('/api/admin/users/hide', {
+        id: user.get('id')
+      }).then(() => {
+        user.set('hidden', true);
+      })
+      .then(null, (err) => {
+        this.notifications.addNotification({
+          message: 'Error: ' + err.responseText,
+          type: 'error'
+        });
+      });
+    },
   }
 });
